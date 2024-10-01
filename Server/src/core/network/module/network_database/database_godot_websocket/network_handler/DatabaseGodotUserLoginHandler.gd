1d4e515d855c1c79706fb4b22a3c41398aa24702
fr4iser90 <pa.boe90@gmail.com>
2024-10-01 21:55:43 +0200

    started user auth, server sends data, to database

--- Server/src/core/network/module/network_database/database_godot_websocket/network_handler/DatabaseGodotUserLoginHandler.gd
+++ Server/src/core/network/module/network_database/database_godot_websocket/network_handler/DatabaseGodotUserLoginHandler.gd
@@ -1,11 +1,30 @@
+# DatabaseUserLoginHandler GODOT
 extends Node
 
+signal login_request_sent
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var websocket_multiplayer_manager
+var is_initialized = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func initialize():
+	if is_initialized:
+		return
+	websocket_multiplayer_manager = GlobalManager.NodeManager.get_cached_node("network_database_module", "network_middleware_manager")
+	is_initialized = true
+	
+# This function sends the login data to the server over WebSocket
+func process_login(client_data: Dictionary, peer_id: int) -> void:
+	var websocket_peer = websocket_multiplayer_manager.get_websocket_peer()
+	if websocket_peer and websocket_peer.get_connection_status() == WebSocketMultiplayerPeer.CONNECTION_CONNECTED:
+		var login_packet = {
+			"type": "user_auth",  # Specify packet type for login
+			"username": client_data.get("username", ""),
+			"password": client_data.get("password", "")
+		}
+		var json_str = JSON.stringify(login_packet)
+		websocket_peer.set_target_peer(1)  # Assuming 1 is the server ID
+		websocket_peer.put_packet(json_str.to_utf8_buffer())
+		print("Sending login data to server: ", json_str)
+		emit_signal("login_request_sent")
+	else:
+		print("WebSocket is not connected, unable to send login data")
